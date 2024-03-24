a8246e18ce035594c70097123289dccd231aa535
anton <antohairt777@gmail.com>
2024-03-24 16:40:10 +0300

    сикма

--- FLask_MAs.py
+++ FLask_MAs.py
@@ -15,6 +15,13 @@ def contacts():
     developer_adres = 'ул. Гузовского, 30, Чебоксары этаж 1'
     return render_template('contacts.html', name=developer_name, adres=developer_adres, phone=developer_nomer)
 
+@app.route("/info/")
+def info():
+    return render_template('info.html')
+
+@app.route("/history/")
+def history():
+    return render_template('history.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
